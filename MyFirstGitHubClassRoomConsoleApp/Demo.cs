c7dee77142d3591a1d6cab68420e43409324ca8b
Niko D <nikodojan@live.de>
2021-08-31 14:24:49 +0200

    first commit

---
+++ MyFirstGitHubClassRoomConsoleApp/Demo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstGitHubClassRoomConsoleApp
+{
+    class Demo
+    {
+        public int Number { get; set; }
+
+        public string Text { get; set; }
+
+        
+    }
+}
